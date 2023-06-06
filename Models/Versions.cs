2cb2ebf13da5c53db50a26b99a43c7a06537531d
“Artem” <1aszx12@mail.ru>
2023-06-06 17:56:32 +0700

    Version Controller is added

---
+++ Models/Versions.cs
@@ -0,0 +1,9 @@
+﻿namespace KlientServ.Models
+{
+    public class Versions
+    {
+        public string Company { get; set; }
+        public string ProductVersion { get; set; }
+        public string Product { get; set; }
+    }
+}
