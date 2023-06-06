8552e9c63f363d666abf9fe62a1876057fd4ef71
“Artem” <1aszx12@mail.ru>
2023-06-06 18:54:08 +0700

    Added storage class resolve to DI config with appsettings.json selection and additional enum

--- Controllers/PhoneController.cs
+++ Controllers/PhoneController.cs
@@ -1,6 +1,8 @@
 ﻿using KlientServ.Models;
+using KlientServ.Storage;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace KlientServ.Controllers
 {
@@ -8,42 +10,64 @@ namespace KlientServ.Controllers
     [ApiController]
     public class PhoneController : ControllerBase
     {
-        private static List<Phone> _MEM = new List<Phone>();
 
+        private IStorage<Phone> _MEM;
+
+        public PhoneController(IStorage<Phone> MemCache)
+        {
+            _MEM = MemCache;
+        }
+        [HttpGet]
         [HttpGet]
         public ActionResult<IEnumerable<Phone>> Get()
         {
-            return _MEM;
+            return Ok(_MEM.All);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Phone> Get(int id)
+        public ActionResult<Phone> Get(Guid id)
         {
-            if (_MEM.Count <= id) throw new IndexOutOfRangeException("Нет такого у нас");
+            if (!_MEM.Has(id)) return NotFound("No such");
 
-            return _MEM[id];
+            return Ok(_MEM[id]);
         }
 
         [HttpPost]
-        public void Post([FromBody] Phone value)
+        public IActionResult Post([FromBody] Phone value)
         {
+            var validationResult = value.Validate();
+
+            if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
+
             _MEM.Add(value);
+
+            return Ok($"{value.ToString()} has been added");
         }
 
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Phone value)
+        public IActionResult Put(Guid id, [FromBody] Phone value)
         {
-            if (_MEM.Count <= id) throw new IndexOutOfRangeException("Нет такого у нас");
+            if (!_MEM.Has(id)) return NotFound("No such");
 
+            var validationResult = value.Validate();
+
+            if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
+
+            var previousValue = _MEM[id];
             _MEM[id] = value;
+
+            return Ok($"{previousValue.ToString()} has been updated to {value.ToString()}");
         }
 
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(Guid id)
         {
-            if (_MEM.Count <= id) throw new IndexOutOfRangeException("Нет такого у нас");
+            if (!_MEM.Has(id)) return NotFound("No such");
 
+            var valueToRemove = _MEM[id];
             _MEM.RemoveAt(id);
+
+            return Ok($"{valueToRemove.ToString()} has been removed");
         }
     }
 }
